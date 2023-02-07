8ebc050a5bf9d7995b9b3ebc7b19698aea62327d
Iain Dillingham <iain@dillingham.me.uk>
2023-02-07 15:13:19 +0000

    Placate sqlfluff

--- analysis/extract_covid_vaccination_name_frequency.sql
+++ analysis/extract_covid_vaccination_name_frequency.sql
@@ -1,5 +1,14 @@
-SELECT VaccinationName_ID, VaccinationName, CEILING(COUNT(VaccinationName) / 100.0) * 100 AS Frequency
+SELECT
+    VaccinationName_ID,
+    VaccinationName,
+    CEILING(COUNT(VaccinationName) / 100.0) * 100 AS Frequency
 FROM Vaccination
-WHERE VaccinationName_ID IN (SELECT VaccinationName_ID FROM VaccinationReference WHERE VaccinationContent = 'SARS-2 Coronavirus')
+WHERE
+    VaccinationName_ID IN (
+        SELECT VaccinationName_ID
+        FROM
+            VaccinationReference
+        WHERE VaccinationContent = 'SARS-2 Coronavirus'
+    )
 GROUP BY VaccinationName_ID, VaccinationName
 ORDER BY Frequency DESC;
