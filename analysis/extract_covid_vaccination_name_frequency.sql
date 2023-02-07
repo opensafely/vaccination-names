d1e285a6f8d6ae06d65e83a56d0e4d750c386854
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-02-07 14:53:19 +0000

    linting; remove trailing comma

--- analysis/extract_covid_vaccination_name_frequency.sql
+++ analysis/extract_covid_vaccination_name_frequency.sql
@@ -1,7 +1,7 @@
-SELECT 
+SELECT
   VaccinationName_ID,
   VaccinationName,
-  CEILING(COUNT(VaccinationName)/100.0)*100 as Frequency,
+  CEILING(COUNT(VaccinationName) / 100.0) * 100 AS Frequency
 FROM Vaccination
 WHERE VaccinationName_ID IN (SELECT VaccinationName_ID FROM VaccinationReference WHERE VaccinationContent = 'SARS-2 Coronavirus')
 GROUP BY VaccinationName_ID, VaccinationName
