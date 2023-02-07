276521f2cd3684a35af967a65e0dc4761d7110a0
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-02-07 14:59:49 +0000

    one line

--- analysis/extract_covid_vaccination_name_frequency.sql
+++ analysis/extract_covid_vaccination_name_frequency.sql
@@ -1,7 +1,4 @@
-SELECT
-  VaccinationName_ID,
-  VaccinationName,
-  CEILING(COUNT(VaccinationName) / 100.0) * 100 AS Frequency
+SELECT VaccinationName_ID, VaccinationName, CEILING(COUNT(VaccinationName) / 100.0) * 100 AS Frequency
 FROM Vaccination
 WHERE VaccinationName_ID IN (SELECT VaccinationName_ID FROM VaccinationReference WHERE VaccinationContent = 'SARS-2 Coronavirus')
 GROUP BY VaccinationName_ID, VaccinationName
