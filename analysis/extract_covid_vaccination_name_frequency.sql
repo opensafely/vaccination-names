fa8bde46dc983f29e9d32a2591241f575f82a17c
wjchulme <25637345+wjchulme@users.noreply.github.com>
2023-02-07 14:35:31 +0000

    add script to report frequency of covid-19 vaccine product names

---
+++ analysis/extract_covid_vaccination_name_frequency.sql
@@ -0,0 +1,8 @@
+SELECT 
+  VaccinationName_ID,
+  VaccinationName,
+  CEILING(COUNT(VaccinationName)/100.0)*100 as Frequency,
+FROM Vaccination
+WHERE VaccinationName_ID IN (SELECT VaccinationName_ID FROM VaccinationReference WHERE VaccinationContent = 'SARS-2 Coronavirus')
+GROUP BY VaccinationName_ID, VaccinationName
+ORDER BY Frequency DESC;
